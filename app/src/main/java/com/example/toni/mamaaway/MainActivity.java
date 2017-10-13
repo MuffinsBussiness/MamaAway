a683507e2d4dc2bebb86305d041a3c18c38014e5
Toni Arellano <toniarellano96@gmail.com>
2017-10-13 23:53:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/toni/mamaaway/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.toni.mamaaway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //myRef.setValue("Hello World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("MOCO", "Values is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("MOCO", "Failed to read value.", databaseError.toException());
+            }
+        });
+    }
+}
